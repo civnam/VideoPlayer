3d24b5f897e2bf3569fa757d6eeeb8c89ffd7364
civnam <109701262+civnam@users.noreply.github.com>
2022-08-18 23:49:34 -0700

    Add files via upload

---
+++ VideoPlayerDemo/VideoPlayerDemo/Components/QueryTag.swift
@@ -0,0 +1,31 @@
+//
+//  QueryTag.swift
+//  VideoPlayerDemo
+//
+//  Created by MAC on 11/08/22.
+//
+
+import SwiftUI
+
+struct QueryTag: View {
+    var query: Query
+    var isSelected: Bool
+    
+    var body: some View {
+        Text(query.rawValue)
+            .font(.callout)
+            .bold()
+            .foregroundColor(isSelected ? .black : .gray)
+            .padding(10)
+            .background(.thinMaterial)
+            .cornerRadius(10)
+    }
+}
+
+/*
+struct QueryTag_Previews: PreviewProvider {
+    static var previews: some View {
+        QueryTag()
+    }
+}
+*/
---
+++ VideoPlayerDemo/VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by MAC on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
